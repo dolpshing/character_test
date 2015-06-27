e2227abf41f0b2396d1335af74f9ebbb09bd463c
A Ryuki Parker <ryuki.parker@gmail.com>
2015-06-26 21:34:26 -0700

    the guy can move!
    arrow keys to move the guy!

--- main.js
+++ main.js
@@ -3,15 +3,41 @@ element.innerText = "";
 var c=document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
 
+c.onkeydown = myFunction;
+
 
 var x = 40;
+var y = 30;
+
+function myFunction(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+        y -= 5;
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        y += 5;
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+       x -= 5;
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       x += 5;
+    }
+    guy();
+}
 
 var guy = function() {
+ ctx.clearRect(0, 0, c.width, c.height);
 
 ctx.beginPath();
 ctx.lineWidth="4";
 ctx.strokeStyle="blue";
-ctx.rect(x-10,30,50,50);
+ctx.rect(x-10,y,50,50);
 ctx.stroke();
 ctx.fillStyle="blue";
 ctx.fill();
@@ -19,7 +45,7 @@ ctx.fill();
 ctx.beginPath();
 ctx.lineWidth="2";
 ctx.strokeStyle="blue";
-ctx.rect(x-8,30,10,10);
+ctx.rect(x-8,y,10,10);
 ctx.stroke();
 ctx.fillStyle="white";
 ctx.fill();
@@ -27,7 +53,7 @@ ctx.fill();
 ctx.beginPath();
 ctx.lineWidth="2";
 ctx.strokeStyle="blue";
-ctx.rect(x+27,30,10,10);
+ctx.rect(x+27,y,10,10);
 ctx.stroke();
 ctx.fillStyle="white";
 ctx.fill();
@@ -35,7 +61,7 @@ ctx.fill();
 ctx.beginPath();
 ctx.lineWidth="2";
 ctx.strokeStyle="blue";
-ctx.rect(x-3,70,40,10);
+ctx.rect(x-3,y+40,40,10);
 ctx.stroke();
 ctx.fillStyle="white";
 ctx.fill();
@@ -43,9 +69,7 @@ ctx.fill();
 
 guy();
 
-var draw = function() {
-  x = x + 10;
-};
+
 
 
 
