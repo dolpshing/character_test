56e9b9fa775dafb1d01c2274807ef6a993e988e5
A Ryuki Parker <ryuki.parker@gmail.com>
2015-06-26 20:37:05 -0700

    Added background and "guy"
    background and variable "guy" added

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ element.innerText = "";
 var c=document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
 
+
+var x = 40;
+
+var guy = function() {
+
 ctx.beginPath();
 ctx.lineWidth="4";
 ctx.strokeStyle="blue";
-ctx.rect(30,30,50,50);
+ctx.rect(x-10,30,50,50);
 ctx.stroke();
 ctx.fillStyle="blue";
 ctx.fill();
@@ -14,7 +19,7 @@ ctx.fill();
 ctx.beginPath();
 ctx.lineWidth="2";
 ctx.strokeStyle="blue";
-ctx.rect(32,30,10,10);
+ctx.rect(x-8,30,10,10);
 ctx.stroke();
 ctx.fillStyle="white";
 ctx.fill();
@@ -22,7 +27,7 @@ ctx.fill();
 ctx.beginPath();
 ctx.lineWidth="2";
 ctx.strokeStyle="blue";
-ctx.rect(67,30,10,10);
+ctx.rect(x+27,30,10,10);
 ctx.stroke();
 ctx.fillStyle="white";
 ctx.fill();
@@ -30,7 +35,17 @@ ctx.fill();
 ctx.beginPath();
 ctx.lineWidth="2";
 ctx.strokeStyle="blue";
-ctx.rect(37,70,40,10);
+ctx.rect(x-3,70,40,10);
 ctx.stroke();
 ctx.fillStyle="white";
 ctx.fill();
+};
+
+guy();
+
+var draw = function() {
+  x = x + 10;
+};
+
+
+
